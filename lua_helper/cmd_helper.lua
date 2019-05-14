c8f74983a1921f720feb1ff6569eb2d3f1a859bf
slore <slorelee@gmail.com>
2019-05-14 23:33:02 +0800

    fix issue that exec() ignores the '/wait' option

--- lua_helper/cmd_helper.lua
+++ lua_helper/cmd_helper.lua
@@ -2,33 +2,33 @@
 
 function option_parser(opt_str)
     local opt = {}
-	opt.wait = 0
-	opt.showcmd = 1
-	if opt_str == nil then return opt end
-	opt_str = opt_str .. ' '
-	if string.find(opt_str, '/nowait ') then opt.wait = 0 end
-	if string.find(opt_str, '/wait ') then opt.wait = 1 end
-	if string.find(opt_str, '/hide ') then
-	    opt.hide = 1
-		opt.showcmd = 0
-	end
-	if string.find(opt_str, '/min ') then
-	    opt.min = 1
-		opt.showcmd = 2
-	end
-	if string.find(opt_str, '/max ') then
-	    opt.max = 1
-		opt.showcmd = 3
-	end
-	return opt
+    opt.wait = false
+    opt.showcmd = 1
+    if opt_str == nil then return opt end
+    opt_str = opt_str .. ' '
+    if string.find(opt_str, '/nowait ') then opt.wait = false end
+    if string.find(opt_str, '/wait ') then opt.wait = true end
+    if string.find(opt_str, '/hide ') then
+        opt.hide = 1
+        opt.showcmd = 0
+    end
+    if string.find(opt_str, '/min ') then
+        opt.min = 1
+        opt.showcmd = 2
+    end
+    if string.find(opt_str, '/max ') then
+        opt.max = 1
+        opt.showcmd = 3
+    end
+    return opt
 end
 
 
 function exec(option, cmd)
     if cmd == nil then
-	    cmd = option
-	    option = nil
-	end
+        cmd = option
+        option = nil
+    end
     local opt = option_parser(option)
     return app:call('exec', cmd, opt.wait, opt.showcmd)
 end
@@ -50,4 +50,3 @@ print(option_parser('/wait /min').showcmd)
 print(option_parser('/wait /max').showcmd)
 print(option_parser(nil).showcmd)
 --]]
-
