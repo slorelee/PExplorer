4a638b5a8aec1d816009f1f2d38229e59507a7c5
slore <slorelee@aliyun.com>
2021-04-02 19:39:19 +0800

    update win_helper.lua

--- lua_helper/win_helper.lua
+++ lua_helper/win_helper.lua
@@ -4,14 +4,38 @@ WM_SYSCOMMAND = 0x0112
 SC_CLOSE      = 0xf060
 
 function HideWindow(class, title)
-  local win = winapi.find_window(class, title)
+  local win
+  if title:find('*') ~= nil then
+    win = winapi.find_window_match(title)
+  else
+    win = winapi.find_window(class, title)
+  end
   if win ~= nil then win:show(winapi.SW_HIDE) end
 end
 
+function MinimizeWindow(class, title)
+  local win
+  if title:find('*') ~= nil then
+    win = winapi.find_window_match(title)
+  else
+    win = winapi.find_window(class, title)
+  end
+  if win ~= nil then win:show(winapi.SW_MINIMIZE) end
+end
+
+
 function CloseWindow(class, title)
-  local win = winapi.find_window(class, title)
-  local hwnd = win:get_handle()
-  app:print('CloseWindow(' .. title .. ', ' .. class .. string.format(", 0x%x", hwnd) .. ')')
-  win:send_message(WM_SYSCOMMAND, SC_CLOSE, 0)
-  return hwnd
+  local win, hwnd
+  if title:find('*') ~= nil then
+    win = winapi.find_window_match(title)
+  else
+    win = winapi.find_window(class, title)
+  end
+  if win ~= nil then 
+    hwnd = win:get_handle()
+    app:print('CloseWindow(' .. title .. ', ' .. class .. string.format(", 0x%x", hwnd) .. ')')
+    win:send_message(WM_SYSCOMMAND, SC_CLOSE, 0)
+    return hwnd
+  end
+  return nil
 end
