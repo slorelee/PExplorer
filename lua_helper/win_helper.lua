35527d2f44a263ae03d33871c6397c966767e1e6
slore <slorelee@aliyun.com>
2021-04-08 23:17:31 +0800

    update win_helper.lua

--- lua_helper/win_helper.lua
+++ lua_helper/win_helper.lua
@@ -3,37 +3,36 @@ require 'winapi'
 WM_SYSCOMMAND = 0x0112
 SC_CLOSE      = 0xf060
 
-function HideWindow(class, title)
+function ShowWindow(class, title, show)
   local win
-  if title:find('*') ~= nil then
+  if title and title:find('*') ~= nil then
     win = winapi.find_window_match(title)
   else
     win = winapi.find_window(class, title)
   end
-  if win ~= nil then win:show(winapi.SW_HIDE) end
+  if show == nil then show = winapi.SW_SHOW end
+  if win ~= nil then win:show(show) end
+end
+
+function HideWindow(class, title)
+  ShowWindow(class, title, winapi.SW_HIDE)
 end
 
 function MinimizeWindow(class, title)
-  local win
-  if title:find('*') ~= nil then
-    win = winapi.find_window_match(title)
-  else
-    win = winapi.find_window(class, title)
-  end
-  if win ~= nil then win:show(winapi.SW_MINIMIZE) end
+  ShowWindow(class, title, winapi.SW_MINIMIZE)
 end
 
 
 function CloseWindow(class, title)
   local win, hwnd
-  if title:find('*') ~= nil then
+  if title and title:find('*') ~= nil then
     win = winapi.find_window_match(title)
   else
     win = winapi.find_window(class, title)
   end
   if win ~= nil then 
     hwnd = win:get_handle()
-    app:print('CloseWindow(' .. title .. ', ' .. class .. string.format(", 0x%x", hwnd) .. ')')
+    app:print('CloseWindow(' .. string.format("%s, %s, 0x%x", title, class, hwnd) .. ')')
     win:send_message(WM_SYSCOMMAND, SC_CLOSE, 0)
     return hwnd
   end
