eed58c6def2ea996686a76c04742b44a463b05bb
slore <slorelee@aliyun.com>
2021-08-04 01:23:12 +0800

    fix issue that CreateMaskLayerWindow() has occupied 200+MB memory

--- features/brightnesslayer.cpp
+++ features/brightnesslayer.cpp
@@ -8,8 +8,6 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 #define GWL_USERDATA        (-21)
 #define SWITCHTOTOP_TIMER_ID 10001
 
-HWND hBrightnessWindow = NULL;
-
 HWND CreateMaskLayerWindow(HINSTANCE hInstance)
 {
     HWND hWnd = 0;
@@ -30,8 +28,11 @@ HWND CreateMaskLayerWindow(HINSTANCE hInstance)
 
     RegisterClass(&wc);
 
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
+
     hWnd = CreateWindow(lpszClassName, MASKLAYERWINDOW_TITLE,
-        WS_POPUP, 0, 0, 8192, 8192,
+        WS_POPUP, 0, 0, 50, 50,
         NULL, NULL, hInstance, NULL);
 
     LONG nRet = ::GetWindowLong(hWnd, GWL_EXSTYLE);
@@ -40,7 +41,7 @@ HWND CreateMaskLayerWindow(HINSTANCE hInstance)
     //00 1A 33 4C 66 80 9A B3 CC E6 FF
     ::SetLayeredWindowAttributes(hWnd, 0, 0, LWA_ALPHA);
     ::SetWindowLong(hWnd, GWL_USERDATA, (LONG)10);
-    ::SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
+    ::SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, width, height, SWP_NOACTIVATE);
     // BringWindowToTop(hWnd);
 
     ::ShowWindow(hWnd, SW_SHOWNORMAL);
@@ -52,12 +53,15 @@ HWND CreateMaskLayerWindow(HINSTANCE hInstance)
 void CreateBrightnessLayer(HINSTANCE hInstance)
 {
     HWND hWnd = NULL;
+
+    // PangolinScreenBrightness.exe
+    if (FindWindow(TEXT("FadeScrClass"), TEXT("Pangolin Screen Brightness")) != NULL) return;
+
     hWnd = FindWindow(MASKLAYERWINDOW_TITLE, NULL);
     if (!hWnd) {
         hWnd = CreateMaskLayerWindow(hInstance);
         if (hWnd) SetTimer(hWnd, SWITCHTOTOP_TIMER_ID, 2000, NULL);
     }
-    hBrightnessWindow = hWnd;
 }
 
 int GetScreenBrightness()
@@ -66,15 +70,35 @@ int GetScreenBrightness()
     if (hWnd) {
         return ::GetWindowLong(hWnd, GWL_USERDATA) * 10;
     }
+    hWnd = FindWindow(TEXT("FadeScrClass"), TEXT("Pangolin Screen Brightness"));
+    if (hWnd) {
+        BYTE     bAlpha;
+        hWnd = FindWindow(TEXT("FadeLensScrClass"), NULL);
+        if (!hWnd) return 100;
+
+        if (GetLayeredWindowAttributes(hWnd, NULL, &bAlpha, NULL)) {
+            int bn_arr[11] = { 0xFF, 0xE6, 0xCC, 0xB3, 0x9A, 0x80, 0x66, 0x4C, 0x33, 0x1A, 0 };
+            for (int i = 0; i < 11; i++) {
+                if (bAlpha >= bn_arr[i])
+                    return i * 10;
+            }
+        }
+    }
     return 100;
 }
 
 int SetScreenBrightness(int brightness)
 {
-    HWND hWnd = FindWindow(MASKLAYERWINDOW_TITLE, NULL);
-    if (!hWnd) hWnd = FindWindow(TEXT("FadeLensScrClass"), NULL);
+    HWND hWnd = NULL;
+    int idx = (int)(brightness / 10);
+    hWnd = FindWindow(MASKLAYERWINDOW_TITLE, NULL);
+    if (!hWnd) {
+        hWnd = FindWindow(TEXT("FadeScrClass"), TEXT("Pangolin Screen Brightness"));
+        idx = 10 - idx;
+    }
+
     if (hWnd) {
-        SendMessage(hWnd, WM_COMMAND, 1000 + (int)(brightness / 10), 0);
+        PostMessage(hWnd, WM_COMMAND, 0x3E8 + idx, 0);
     }
     return 0;
 }
@@ -115,8 +139,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             }
         }
     }
-
     break;
+    case WM_DISPLAYCHANGE: {
+        int w = GetSystemMetrics(SM_CXSCREEN);
+        int h = GetSystemMetrics(SM_CYSCREEN);
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, w, h, SWP_NOACTIVATE);
+        break;
+    }
     case WM_DESTROY:
         ::PostQuitMessage(0);
         break;
