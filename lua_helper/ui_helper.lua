97c7dd52a8cf0cf98936c3388fb84a6baafa9221
slore <slorelee@aliyun.com>
2022-08-30 15:34:28 +0800

    update ui_helper.lua

--- lua_helper/ui_helper.lua
+++ lua_helper/ui_helper.lua
@@ -1,6 +1,7 @@
 UI = {}
 UI.OnClick = {}
 UI.OnChanged = {}
+UI.OnTimer = {}
 
 function UI.UpdateText(name)
     sui:find(name).text = sui:find(name).text
@@ -35,10 +36,13 @@ function clsUITab:Click(tab)
 end
 
 function clsUITab:OnClick(tab, ctrl)
-    App:Debug('clsUITab:OnClick - ' .. tab  .. ' ' .. ctrl)
+    App:Debug('UITab:OnClick - ' .. tab  .. ' ' .. tostring(ctrl))
     self.Layout.selectedid = self.List[tab]
 end
 
+clsUITab.DefOnClick = clsUITab.OnClick
+
+---
 UITab = {}
 function UITab.New(name)
     local o = {}
@@ -97,8 +101,90 @@ function UISwitch.SetText(elem, val)
     if val == 0 then elem.text = "%{Off}" else elem.text = "%{On}" end
 end
 
+
+---
+UIDispatcher = {}
+
 UIEvent = {}
-function UIEvent.OnClick(ctrl)
+UIEvent.Trigger = true
+
+UIEvent.OnTimer = nil
+UIEvent.OnClick = nil
+UIEvent.OnChanged = nil
+
+local function OverRideFunc(funcs, ...)
+    for i = 1, #funcs do
+        if type(funcs[i]) == 'function' then
+            funcs[i](...)
+            return 1
+        end
+    end
+    return nil
+end
+
+---
+-- TID_APP = 10000
+-- TID_AUTO = 20000
+-- TID_USER = 30000
+
+UITimer = {}
+UITimerId = {}
+UITimerStrId = {}
+UITimer.NextId = TID_AUTO
+
+function UI:SetTimer(tid, interval)
+    local strid = tostring(tid)
+    local timerid = UITimerId[strid]
+
+    -- not exist
+    if timerid == nil then
+        if math.type(tid) == 'integer' then
+            timerid = tid
+        else
+            -- auto timerid
+            timerid = UITimer.NextId
+            UITimer.NextId = UITimer.NextId + 1 
+        end
+
+        UITimerId[strid] =  timerid
+        UITimerStrId[timerid] = strid
+    end
+
+    App:Debug("[DEBUG] UI:SetTimer(" .. timerid .. "['" .. strid .. "'] ," .. interval .. ")")
+    if strid:sub(1, 6) == 'RESET.' then
+        UI:KillTimer(strid)
+    end
+    suilib.call('SetTimer', timerid, interval)
+end
+
+function UI:KillTimer(tid)
+    local timerid = UITimerId[tostring(tid)]
+    if timerid ~= nil then
+        suilib.call('KillTimer', timerid)
+    end
+end
+
+function UIDispatcher.OnTimer(tid)
+    local strid = UITimerStrId[tid]
+    if strid == nil then
+        App:Info("[INFO] The UITimer[" .. tid .."] is not defined.")
+    elseif type(UI.OnTimer[strid]) == "function" then
+        if strid:sub(1, 6) == 'RESET.' then
+            UI:KillTimer(strid)
+        end
+        UI.OnTimer[strid](tid)
+        return
+    end
+
+    return OverRideFunc({UIEvent.OnTimer, ontimer}, tid)
+end
+
+---
+
+function UIDispatcher.OnClick(ctrl)
+    if UIWindow.Inited == 0 then return 0 end
+    if UIEvent.Trigger ~= true then return 0 end
+
     local obj, group, func, param1
     if ctrl:sub(1, 1) == "$" then
         group = ctrl:match("($.+)%[")
@@ -116,6 +202,7 @@ function UIEvent.OnClick(ctrl)
     func = UI.OnClick[ctrl]
     if func == nil and group ~= nil then func = UI.OnClick[group] end
     if type(func) == 'function' then
+        App:Debug("[DEBUG] UIEvent.OnClick[" .. ctrl .."] function()")
         if param1 then
             func(param1, ctrl)
         else
@@ -123,12 +210,17 @@ function UIEvent.OnClick(ctrl)
         end
         return 1
     end
-    return nil
+
+    return OverRideFunc({UIEvent.OnClick, onclick}, ctrl)
 end
 
-function UIEvent.OnChanged(ctrl, val)
+
+function UIDispatcher.OnChanged(ctrl, val)
+    if UIWindow.Inited == 0 then return 0 end
+    if UIEvent.Trigger ~= true then return 0 end
+
     local func = UI.OnChanged[ctrl]
-    App:Print('UIEvent.OnChanged', ctrl, val)
+    App:Debug('[DEBUG] UIEvent.OnChanged', ctrl, val)
     if ctrl:sub(1, 8) == '$Switch.' then
         UISwitch.SetText(ctrl, val)
     end
@@ -136,5 +228,62 @@ function UIEvent.OnChanged(ctrl, val)
         func(val, ctrl)
         return 1
     end
-    return nil
+
+    return OverRideFunc({UIEvent.OnChanged, onchanged}, ctrl, val)
+end
+
+function UIDispatcher.OnReturn(ctrl)
+    return OverRideFunc({UIEvent.OnReturn, onreturn}, ctrl)
+end
+
+function UIDispatcher.OnLink(ctrl)
+    return OverRideFunc({UIEvent.OnLink, onlink}, ctrl)
+end
+
+function UIDispatcher.OnFocus(ctrl)
+    return OverRideFunc({UIEvent.OnFocus, onfocus}, ctrl)
+end
+
+function UIDispatcher.OnHover(ctrl)
+    return OverRideFunc({UIEvent.OnFocus, onhover}, ctrl)
+end
+
+function UIDispatcher.OnHoverChanged(ctrl)
+    return OverRideFunc({UIEvent.OnFocus, onhoverchanged}, ctrl)
+end
+
+function UIDispatcher.OnDeactive(ctrl)
+    if UIWindow.Inited == 0 then return 0 end
+    return OverRideFunc({UIEvent.OnDeactive, ondeactive}, ctrl)
+end
+
+function UIDispatcher.OnMessage(msg, wparam, lparam)
+    return OverRideFunc({UIEvent.OnMessage, onmessage}, msg, wparam, lparam)
+end
+
+---
+UIWindow = {}
+UIWindow.Inited = 0
+
+function UIWindow:Init()
+    -- for backward compatibility
+    if type(init) == 'function' then
+        init()
+    end
+end
+
+function UIWindow:OnLoad()
+    -- for backward compatibility
+    if type(onload) == 'function' then
+        onload()
+    end
+
+    UIWindow.Inited = 1
+end
+
+function UIWindow:OnShow()
+    -- for backward compatibility
+    if type(onshow) == 'function' then
+        onshow()
+    end
 end
